3c940e874d6e91089aa2d5aa40dddaac7595115b
Mlattard <maxime@lattard.fr>
2023-03-19 22:38:32 -0400

    Advancement
    - Rectangle basics
    - Input

--- asteroid_shooter.py
+++ asteroid_shooter.py
@@ -3,12 +3,11 @@ import pygame, sys
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-pygame.display.set_caption("Asteroid shooter")
+pygame.display.set_caption("Poew Poew Poew")
 clock = pygame.time.Clock()
 
 ship_surf = pygame.image.load('graphics/ship.png').convert_alpha()
 ship_rect = ship_surf.get_rect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
-
 bckgr_surf = pygame.image.load('graphics/background.png').convert()
 
 font = pygame.font.Font('graphics/subatomic.ttf', 50)
@@ -24,12 +23,10 @@ while True:
 
     clock.tick(120)
 
+    ship_rect.center = pygame.mouse.get_pos()
+
     display_surface.fill((0, 0, 0))
     display_surface.blit(bckgr_surf, (0, 0))
-    
-    if ship_rect.top - 4 > 0:
-        ship_rect.y -= 4
-
     display_surface.blit(ship_surf, ship_rect)
     display_surface.blit(text_surf, text_rect)
    
