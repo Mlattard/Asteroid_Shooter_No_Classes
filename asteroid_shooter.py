bd68243ccf9c961c597b94a7e4e00962e8df4bf8
Mlattard <maxime@lattard.fr>
2023-03-19 22:18:32 -0400

    Files import and project update
    - Images and Text
    - Movement

--- asteroid_shooter.py
+++ asteroid_shooter.py
@@ -4,6 +4,16 @@ pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Asteroid shooter")
+clock = pygame.time.Clock()
+
+ship_surf = pygame.image.load('graphics/ship.png').convert_alpha()
+ship_rect = ship_surf.get_rect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+
+bckgr_surf = pygame.image.load('graphics/background.png').convert()
+
+font = pygame.font.Font('graphics/subatomic.ttf', 50)
+text_surf = font.render('Space', True, 'White')
+text_rect = text_surf.get_rect(midbottom = (WINDOW_WIDTH / 2, WINDOW_HEIGHT - 80))
 
 while True:
 
@@ -12,6 +22,15 @@ while True:
             pygame.quit()
             sys.exit()
 
-    display_surface.fill((0, 0, 0))
+    clock.tick(120)
 
+    display_surface.fill((0, 0, 0))
+    display_surface.blit(bckgr_surf, (0, 0))
+    
+    if ship_rect.top - 4 > 0:
+        ship_rect.y -= 4
+
+    display_surface.blit(ship_surf, ship_rect)
+    display_surface.blit(text_surf, text_rect)
+   
     pygame.display.update()
